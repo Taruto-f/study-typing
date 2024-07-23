8fb2975056e7f6a5e3656205fd5c2354d9a25f26
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-07-23 13:18:47 +0900

    テスト強化 Fixes #10

---
+++ test/util/only_enable.test.ts
@@ -0,0 +1,41 @@
+import { only_enable } from '@/app/util';
+import { faker } from '@faker-js/faker';
+
+test('該当なし', () => {
+  const dat = new Set<string>(['a', 'b', 'c']);
+  const enable: Record<string, boolean> = {
+    a: false,
+    b: false,
+    c: false,
+  };
+
+  expect(only_enable(dat, enable)).toStrictEqual(new Set<string>());
+});
+
+test('全部', () => {
+  const dat = new Set<string>(['a', 'b', 'c']);
+  const enable: Record<string, boolean> = {
+    a: true,
+    b: true,
+    c: true,
+  };
+
+  expect(only_enable(dat, enable)).toStrictEqual(dat);
+});
+
+test('ランダム', () => {
+  const dat = new Set<string>();
+  const ans = new Set<string>();
+  const enable: Record<string, boolean> = {};
+  for (let i = 0; i < 100; ++i) {
+    const key = faker.string.alpha(20);
+    dat.add(key);
+
+    const enab = faker.datatype.boolean();
+    if (enab) ans.add(key);
+
+    enable[key] = enab;
+  }
+
+  expect(only_enable(dat, enable)).toStrictEqual(ans);
+});
