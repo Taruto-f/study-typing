e9d58a71f5e6921dce50c0825b8bc05b630545fc
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-09-06 23:23:19 +0900

    ui fix

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -118,7 +118,7 @@ function ShowTable({
               isCompact
               showControls
               showShadow
-              color='secondary'
+              color='primary'
               page={page}
               total={pages}
               onChange={(page) => setPage(page)}
