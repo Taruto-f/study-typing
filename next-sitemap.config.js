f7d58c68dc2d21fcfd3bd7023900d2284a5c374d
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-01-09 23:05:40 +0900

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 const config={
 	siteUrl:"https://study-typing.vercel.app",
-	generateIndexSitemap:true,
+	generateIndexSitemap:false,
 	generateRobotsTxt:true,
 	exclude:["/play","/share","/data","/icon.svg"]
 
